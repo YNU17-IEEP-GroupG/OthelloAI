952de00fb4aab06f4bd2cadefac37c7c7d4e9d41
yuta-takanashi <takanashi-yuta-rs@ynu.jp>
2017-05-10 22:38:01 +0900

    AIはこれから複数作るので、AIのインターフェースを定義

---
+++ src/BaseAI.java
@@ -0,0 +1,8 @@
+/**
+ * Created by shiita on 2017/05/10.
+ */
+public interface BaseAI {
+    void think();
+    int getRow();
+    int getColumn();
+}
--- src/RandomAI.java
+++ src/RandomAI.java
@@ -4,7 +4,7 @@ import java.util.Random;
 /**
  * Created by shiita on 2017/04/29.
  */
-public class RandomAI {
+public class RandomAI implements BaseAI{
     private List<Point> hint;
     private int row;
     private int column;
@@ -13,19 +13,21 @@ public class RandomAI {
         this.hint = hint;
         row = hint.get(0).row;
         column = hint.get(0).column;
-        think();
     }
 
-    private void think() {
+    @Override
+    public void think() {
         Point point = hint.get(new Random().nextInt(hint.size()));
         row = point.row;
         column = point.column;
     }
 
+    @Override
     public int getRow() {
         return row;
     }
 
+    @Override
     public int getColumn() {
         return column;
     }
