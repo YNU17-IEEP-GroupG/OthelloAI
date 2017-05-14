e33d8696ac1e47c097e3b2b7daf55acbc032ae22
yuta-takanashi <takanashi-yuta-rs@ynu.jp>
2017-05-15 01:36:25 +0900

    ビームサーチの実装。相手の手の絞込がうまくいかないので弱い

--- src/ai/Search.java
+++ src/ai/Search.java
@@ -4,23 +4,91 @@ import util.BoardHelper;
 import util.Point;
 import util.Stone;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by shiita on 2017/05/13.
  */
 public class Search {
     private Stone targetStone;
-
+    // TODO: typeをフィールドに
     public Search(Stone targetStone) {
         this.targetStone = targetStone;
     }
 
+    public Point beamSearch(Evaluation.EvaluationType type, Stone[][] board, int limit) {
+        List<Status> evaluated = new ArrayList<>();
+        List<Status> beam;
+
+        // beamの初期化
+        beam = calcNewStatus(new Status(0, new ArrayList<>(), targetStone.getReverse(), board), type);
+        beam = updateBeam(beam, limit);
+        while (!beam.isEmpty()) {
+            int size = beam.size();
+            for (int j = 0; j < size; j++) {
+                Status s = beam.get(0);
+                beam.remove(0);
+                if (s.stone == targetStone) {
+                    evaluated.add(s);
+                }
+                s = new Status(s.value, s.path, s.stone.getReverse(), s.board);
+                List<Status> newS = calcNewStatus(s, type);
+                beam.addAll(newS);
+            }
+
+            beam = updateBeam(beam, limit);
+        }
+        evaluated.forEach(status -> {
+            System.out.print("value = " + status.value);
+            status.printPath();
+            System.out.println();
+        });
+        evaluated = evaluated.stream()
+                .sorted(Comparator.comparing(Status::getValue, Comparator.reverseOrder()))
+                .collect(Collectors.toList());
+        System.out.println("last value =" +  evaluated.get(0).value);
+        return evaluated.get(0).path.get(0);
+    }
+
+    private List<Status> calcNewStatus(Status status, Evaluation.EvaluationType type) {
+        Stone reverse = status.stone.getReverse();
+        List<Status> list = new ArrayList<>();
+        List<Point> hint = BoardHelper.makeHint(reverse, status.board);
+        for (Point point : hint) {
+            Stone[][] newBoard = BoardHelper.cloneBoard(status.board);
+            BoardHelper.putStone(point.row, point.column, reverse, newBoard);
+            int value = evaluate(type, reverse, newBoard);
+            List<Point> path = new ArrayList<>();
+            for (Point p : status.path) path.add(p);
+            path.add(point);
+            list.add(new Status(value, path, reverse, newBoard));
+        }
+        return list;
+    }
+
+    private List<Status> updateBeam(List<Status> beam, int limit) {
+        int l = Math.min(beam.size(), limit);
+        if (l == 0) return new ArrayList<>();
+        if (beam.get(0).stone == targetStone)
+            return beam.stream()
+                .sorted(Comparator.comparing(Status::getValue, Comparator.naturalOrder()))
+                .limit(l)
+                .collect(Collectors.toList());
+        else
+            return beam.stream()
+                    .sorted(Comparator.comparing(Status::getValue, Comparator.reverseOrder()))
+                    .limit(l)
+                    .collect(Collectors.toList());
+    }
+
     public Point untilEnd(Evaluation.EvaluationType type, Stone stone, Stone[][] board) {
-        Point bestPoint = new Point(0,0);
         Triple bestTriple = new Triple(-1, 0, 0);
         Triple triple;
         List<Point> hint = BoardHelper.makeHint(stone, board);
+        Point bestPoint = hint.get(0);
         for (Point p : hint) {
             List<Point> pList = BoardHelper.putStone(p.row, p.column, stone, board);
             triple = uEnd(type, stone.getReverse(), board);
@@ -152,6 +220,28 @@ public class Search {
         public int value;
         public Triple(double winPer, int count, int value) {
             this.winPer = winPer; this.count = count; this.value = value;
+        }
+    }
+
+    private class Status {
+        public int value;
+        public List<Point> path;
+        public Stone stone;
+        public Stone[][] board;
+        public Status(int value, List<Point> path, Stone stone, Stone[][] board) {
+            this.value = value;
+            this.path = path;
+            this.stone = stone;
+            this.board = board;
+        }
+        public int getValue() {
+            return value;
+        }
+
+        public void printPath() {
+            path.stream().forEach(p -> {
+                System.out.print("(" + p.row + ", " + p.column + "), ");
+            });
         }
     }
 }
